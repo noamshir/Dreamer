8e25e81a5bf38ec98de1ac28e180ac5d238f51b6
Noam Shir <noamshirm@gmail.com>
2022-01-19 13:40:22 +0200

    added header

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
+import { AppHeader } from './cmp/AppHeader.jsx';
 import routes from './routes.js'
 
 export class App extends React.Component {
 
     render() {
         return (
-            <div className="app">
-                <main className="app-main">
+            <div className="main-wrapper">
+                <AppHeader />
+                <main className="main-content">
                     <Switch>
                         {routes.map(route => <Route key={route.path} exact component={route.component} path={route.path} />)}
                     </Switch>
--- front-end/src/cmp/AppHeader.jsx
+++ front-end/src/cmp/AppHeader.jsx
@@ -1,21 +1,23 @@
 
 
+import MenuIcon from '@mui/icons-material/Menu';
+
 export function AppHeader() {
 
     return <section className="main-header sticky">
-        <div className="Header">
+        <div id="Header">
             <header className="header-package dreamer-header header-transparent logged-out-homepage-header">
                 <div className="header-row-wrapper">
                     <div className="header-row max-width-container equal-padding row-main flex">
-                        <button className="btn-nav">Nav</button>
-                        <a href="" className="dreamer-logo"></a>
+                        <button className="btn-nav"><MenuIcon></MenuIcon></button>
+                        <a href="" className="dreamer-logo clean-link">Dreamer</a>
                         <div className="dreamer-header-search-animated"> </div>
                         <nav className="dreamer-nav dreamer-nav-right flex">
                             <ul className="flex">
-                                <li className="display-from-size"><a href="">1</a></li>
-                                <li className="display-from-size"><a href="">2</a></li>
-                                <li className="display-from-size"><a href="">3</a></li>
-                                <li className="display-from-size"><a href="">4</a></li>
+                                <li className="display-from-size"><a href="" className="clean-link">1</a></li>
+                                <li className="display-from-size"><a href="" className="clean-link">2</a></li>
+                                <li className="display-from-size"><a href="" className="clean-link">3</a></li>
+                                <li className="display-from-size"><a href="" className="clean-link">4</a></li>
                             </ul>
                         </nav>
                     </div>
