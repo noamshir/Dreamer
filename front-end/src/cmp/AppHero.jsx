11f9ea67626c99525f7c25d692dcba2c1395ec16
Itamar Avady <itamaravady@gmail.com>
2022-01-19 19:04:22 +0200

    hero layout

--- front-end/src/cmp/AppHero.jsx
+++ front-end/src/cmp/AppHero.jsx
@@ -7,8 +7,16 @@ import { HeroPopularCategory } from './HeroPopularCategory'
 export function AppHero(props) {
     return (
         <div className="hero-container">
-            <div className='background-images'></div>
-            <div className="hero-content-container main-container">
+            <div className='background-images'>
+                {/* <div className='background'><img src="https://res.cloudinary.com/drdfrwt1d/image/upload/v1642608521/hero-portrait_3_y5jms3.png" /></div> */}
+                {/* <div className='background'><img src="https://res.cloudinary.com/drdfrwt1d/image/upload/v1642608521/hero-portrait_4_gfsdbz.png" /></div> */}
+                {/* <div className='background'><img src="https://res.cloudinary.com/drdfrwt1d/image/upload/v1642608522/hero-portrait_2_ifwtha.png" /></div> */}
+                <div className='background'>
+                    <img src="https://res.cloudinary.com/drdfrwt1d/image/upload/v1642608521/hero-portrait_1_kxdfkl.png" />
+                    <span className='seller-name'>Mark, <span className='seller-skill'>Illustrator</span></span>
+                </div>
+            </div>
+            <div className="hero-content-container">
                 <div className='hero-content'>
                     <h1>Find the perfect <i>freelance</i> services for your business</h1>
                     <SearchBar />
