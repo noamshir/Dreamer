fc75d2549efc62f7ccabb0ffa3a3b9f6953b471c
Ben Haroosh <bharoosh@gmail.com>
2022-01-19 13:39:16 +0200

    start of explore

--- front-end/src/routes.js
+++ front-end/src/routes.js
@@ -1,9 +1,14 @@
+import { Explore } from "./pages/Explore.jsx";
 import { Home } from "./pages/Home.jsx";
 
 const routes = [
   {
     path: "/",
     component: Home,
+  },
+  {
+    path: "/explore",
+    component: Explore,
   },
 ];
 
