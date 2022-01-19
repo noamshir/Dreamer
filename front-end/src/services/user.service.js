2a36f47f8cb9e25916010d8b54dfb334ff6fbe6c
Ben Haroosh <bharoosh@gmail.com>
2022-01-19 10:18:11 +0200

    commit

--- front-end/src/services/user.service.js
+++ front-end/src/services/user.service.js
@@ -6,7 +6,7 @@ var axios = Axios.create({
     withCredentials: true
 })
 
-const STORAGE_KEY = 'user'
+const STORAGE_KEY = 'user_db'
 const STORAGE_KEY_LOGGEDIN = 'loggedinUser'
 
 export const userService = {
@@ -21,28 +21,29 @@ export const userService = {
 window.us = userService
 
 async function login(credentials) {
-    const user = await httpService.post('auth/login', credentials)
+    // const user = await httpService.post('auth/login', credentials)
+    // if (user) sessionStorage.setItem(STORAGE_KEY_LOGGEDIN, JSON.stringify(user))
+    // return user
+
+    const users = storageService.query(STORAGE_KEY)
+    const user = users.find(user => user.username === credentials.username &&
+        user.password === credentials.password)
     if (user) sessionStorage.setItem(STORAGE_KEY_LOGGEDIN, JSON.stringify(user))
     return user
-
-    // const users = storageService.query(STORAGE_KEY)
-    // const user = users.find(user => user.username === credentials.username &&
-    //     user.password === credentials.password)
-    // return user
 }
 async function signup(userInfo) {
-    const user = await httpService.post('auth/signup', userInfo)
-    if (user) sessionStorage.setItem(STORAGE_KEY_LOGGEDIN, JSON.stringify(user))
-    return user
-    // const user = await storageService.post(STORAGE_KEY, userInfo)
-    // sessionStorage.setItem(STORAGE_KEY_LOGGEDIN, JSON.stringify(user))
+    // const user = await httpService.post('auth/signup', userInfo)
+    // if (user) sessionStorage.setItem(STORAGE_KEY_LOGGEDIN, JSON.stringify(user))
     // return user
+    const user = await storageService.post(STORAGE_KEY, userInfo)
+    sessionStorage.setItem(STORAGE_KEY_LOGGEDIN, JSON.stringify(user))
+    return user
 }
 async function logout() {
-    const data = await httpService.post('auth/logout')
+    // const data = await httpService.post('auth/logout')
+    // return data
     sessionStorage.setItem(STORAGE_KEY_LOGGEDIN, null)
-    return data
-    // return Promise.resolve()
+    return Promise.resolve()
 }
 
 function getLoggedinUser() {
@@ -78,17 +79,65 @@ async function getById(userId) {
 
 
 
-function createUsers() {
+function _createUsers() {
     return [
         {
-            "_id": "u101",
+            "_id": "u108",
+            "fullname": "Baadur Lomidze",
+            "imgUrl": "assets/img/user108.jpg",
+            "isAdmin": false,
+            "username": "BaadurL",
+            "password": "BaadurLomidze",
+            "sellerInfo": {
+                "level": "level 1",
+                "origin": "Israel",
+                "languages": ["eng", "heb"],
+                "skills": [
+                    "logo-design",
+                    "programming"
+                ]
+            },
+            //should we seperate the reviews to a different json
+            //and include here only a reviews id array???????
+            "reviews":
+                [
+                    {
+                        "id": "r101",
+                        "txt": "Very kind and works fast",
+                        "rate": 4,
+                        "by": {
+                            "_id": "u102",
+                            "fullname": "user2",
+                            "imgUrl": "/img/img2.jpg"
+                        }
+                    }
+                ]
+            ,
+            likedGigs: [
+                {
+                    "_id": "i101",
+                    "name": "Design Logo",
+                    "imgUrl": "url1",
+                    "price": 20
+                }
+            ]
+        },
+        {
+            "_id": "u107",
             "fullname": "Dwayne Loony",
             "imgUrl": "assets/imgs/user107.jpg",
             "isAdmin": false,
-            "isSeller": true,
             "username": "BestSellerXo",
             "password": "secret",
-            "level": "level 2",
+            "sellerInfo": {
+                "level": "level 1",
+                "origin": "Israel",
+                "languages": ["eng", "heb"],
+                "skills": [
+                    "logo-design",
+                    "programming"
+                ]
+            },
             "reviews": [
                 {
                     "id": "madeId",
