2e3ff876dd09685d10b54a96144f7c68707046eb
Ben Haroosh <bharoosh@gmail.com>
2022-01-20 12:19:29 +0200

    starting details

--- front-end/src/cmp/GigPreview.jsx
+++ front-end/src/cmp/GigPreview.jsx
@@ -1,15 +1,25 @@
 import StarIcon from '@mui/icons-material/Star';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import { useState } from 'react';
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom';
+
 
 import { Carousel } from "./Carousel";
 import { CarouselItem } from "./CarouselItem";
+import { setLikedGig } from '../store/gig.action.js'
+import { storageService } from '../services/async-storage.service';
 
 
-
-export function GigPreview({ gig }) {
+function _GigPreview({ gig, onGoToDetails, user, setLikedGig }) {
     const [likeMsgClass, setClass] = useState(false)
+    const [isLiked, setLiked] = useState(false)
+    checkIfLiked()
 
+    async function checkIfLiked() {
+        const isGuestLiked = await storageService.isLikedByGuest(gig._id)
+        setLiked(isGuestLiked)
+    }
 
     function getUserLevel() {
         if (gig.owner.rate === 1 || gig.owner.rate < 3) {
@@ -22,16 +32,19 @@ export function GigPreview({ gig }) {
         return gig.owner.level = 'Top Rated Seller'
     }
 
+    async function toggleLike() {
+        setLikedGig(gig, user)
+        setLiked(!isLiked)
+    }
+
     return (
         <section className='gig-preview'>
             <div className='card'>
-                {/* <div className="" > */}
                 <div className="gig-img">
-                    <Carousel>
+                    <Carousel onGoToDetails={onGoToDetails} gig={gig}>
                         {gig.imgUrls.map((imgUrl, idx) => <CarouselItem key={idx} imgUrl={imgUrl}></CarouselItem>)}
                     </Carousel>
                 </div>
-                {/* </div> */}
                 <div className="owner-info">
                     <div className='owner-pic' style={{ backgroundImage: `url(${gig.owner.imgUrl})` }}></div>
                     <div className="owner-level-name">
@@ -39,25 +52,47 @@ export function GigPreview({ gig }) {
                         <h5 className='owner-level'>{getUserLevel()}</h5>
                     </div>
                 </div>
-                <p className='gig-title'>{gig.title}</p>
+                <Link className='clean-link' to={`/explore/${gig._id}`}>
+                    <p className='gig-title'>{gig.title}</p>
+                </Link>
                 <div className='rate-wrapper'>
                     <span className='gig-rating'><StarIcon /> {gig.owner.rate}<span className='review-number'>(1k+)</span></span>
                 </div>
                 <div className="price-like-container">
 
-                    <span className='popup like' onMouseLeave={() => {
-                        setClass(false)
-                    }} onMouseEnter={() => {
-                        setClass(true)
-                    }}><FavoriteIcon />
-                        <span className={likeMsgClass ? 'popuptext show' : 'popuptext'}> add to favorite</span>
+                    <span className={isLiked ? 'popup like active' : 'popup like'}
+                        onMouseLeave={() => {
+                            setClass(false)
+                        }} onMouseEnter={() => {
+                            setClass(true)
+                        }}
+                        onClick={toggleLike}
+                    ><FavoriteIcon />
+                        <span className={likeMsgClass ? 'popuptext show' : 'popuptext'}> {isLiked ? 'added' : 'add to favorite'}</span>
                     </span>
-                    <ul className='clean-list'>
-                        <li className='starting-price'>Starting At</li>
-                        <li>{gig.price.toLocaleString("ILS", { style: "currency", currency: "ILS" })}</li>
-                    </ul>
+                    <Link className='clean-link' to={`/explore/${gig._id}`}>
+                        <ul className='clean-list'>
+                            <li className='starting-price'>Starting At</li>
+                            <li>{gig.price.toLocaleString("ILS", { style: "currency", currency: "ILS" })}</li>
+                        </ul>
+                    </Link>
                 </div>
             </div>
         </section >
     )
 }
+
+
+function mapStateToProps(state) {
+    return {
+        gigs: state.gigModule.gigs,
+        user: state.userModule.user
+    }
+}
+
+const mapDispatchToProps = {
+    setLikedGig
+};
+
+
+export const GigPreview = connect(mapStateToProps, mapDispatchToProps)(_GigPreview)
--- front-end/src/pages/GigDetails.jsx
+++ front-end/src/pages/GigDetails.jsx
@@ -1,21 +1,75 @@
 import { connect } from 'react-redux'
-import { useState } from 'react'
-import { onSetFilterBy } from '../store/gig.action'
+import React from 'react'
+import StarIcon from '@mui/icons-material/Star';
 
-function _GigDetails(props) {
 
-    return (
-        <div>Details</div>
-    )
+import { onSetFilterBy } from '../store/gig.action.js'
+import { gigService } from '../services/gig.service.js'
+
+class _GigDetails extends React.Component {
+    state = {
+        gig: null
+    }
+    stars = [<StarIcon />, <StarIcon />, <StarIcon />, <StarIcon />, <StarIcon />]
+
+    async componentDidMount() {
+        const { gigId } = this.props.match.params
+        this.loadGig(gigId)
+    }
+
+    loadGig = async (gigId) => {
+        const gig = await gigService.getById(gigId)
+        // console.log('gig in details', gig);
+        this.setState(prevState => ({ ...prevState, gig }))
+    }
+
+    get getUserLevel() {
+        const { gig } = this.state
+        if (gig.owner.rate === 1 || gig.owner.rate < 3) {
+            return gig.owner.level = 'New Seller'
+        } else if (gig.owner.rate < 4) {
+            return gig.owner.level = 'Level 1'
+        } else if (gig.owner.rate < 4.5) {
+            return gig.owner.level = 'Level 2'
+        }
+        return gig.owner.level = 'Top Rated Seller'
+    }
+
+    render() {
+        const { gig } = this.state
+        if (!gig) return <React.Fragment></React.Fragment>
+        return (
+            <section className='gig-details'>
+                <header>
+
+                </header>
+                <h1>{gig.title}</h1>
+                <div className="owner-info">
+                    <div className='owner-img' style={{ backgroundImage: `url(${gig.owner.imgUrl})` }}></div>
+                    <h5 className='owner-name'>{gig.owner.fullname}</h5>
+                    <h5 className='owner-level'>{this.getUserLevel}</h5>
+                    <span>|</span>
+                    {this.stars.map((star, idx) => {
+                        if (idx < (gig.owner.rate - 1)) return <span key={idx} className='star active'>{star}</span>
+                        else return <span key={idx} className='star'>{star}</span>
+                    })}
+                </div>
+            </section>
+        )
+
+    }
 }
 
 
 function mapStateToProps(state) {
-    return {}
+    return {
+        gigs: state.gigModule.gigs,
+        user: state.userModule.user
+    }
 }
 
 const mapDispatchToProps = {
-    onSetFilterBy
+    onSetFilterBy,
 }
 
 export const GigDetails = connect(mapStateToProps, mapDispatchToProps)(_GigDetails)
