4a942513c76b65c4e81f31a21c364077429e2bf5
Noam Shir <noamshirm@gmail.com>
2022-01-23 09:38:46 +0200

    push reviews

--- front-end/src/cmp/ReviewItem.jsx
+++ front-end/src/cmp/ReviewItem.jsx
@@ -1,11 +1,13 @@
 export const ReviewItem = ({ review, width, imgUrl }) => {
     return (
-        <div className="carousel-item" style={{ width: width }}>
+        <div className="review-item" style={{ width: width }}>
             <img src={imgUrl} alt="" />
             <div className="review-content">
+                <div className="wrap-text">
                 <h5>{review.writers}</h5>
                 <h4>{review.company}</h4>
                 <p>{review.txt}</p>
+                </div>
             </div>
         </div>
     );
