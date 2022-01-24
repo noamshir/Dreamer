b83bd31d481e04d31791fafea3e0f940da0c0831
Noam Shir <noamshirm@gmail.com>
2022-01-24 09:38:37 +0200

    new commit

---
+++ front-end/src/cmp/Menu.jsx
@@ -0,0 +1,18 @@
+
+
+export function Menu() {
+   console.log("render menu")
+    return <section className="side-Bar">
+        <div className="side-bar-content">
+            <header className="menu-header"></header>
+            <nav className="menu-nav">
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </nav>
+        </div>
+    </section>
+}
