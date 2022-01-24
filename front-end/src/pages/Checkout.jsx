94de9d94766a97701fb2b78c4892efca7ff0020b
Ben Haroosh <bharoosh@gmail.com>
2022-01-24 13:42:00 +0200

    finished scss order

--- front-end/src/pages/Checkout.jsx
+++ front-end/src/pages/Checkout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { UserStarRate } from "../cmp/Details/UserStarRate";
+import CheckIcon from '@mui/icons-material/Check';
 
+import { UserStarRate } from "../cmp/Details/UserStarRate";
 
 import { gigService } from "../services/gig.service";
 import { userService } from "../services/user.service";
@@ -15,8 +16,16 @@ export class Checkout extends React.Component {
 
     async componentDidMount() {
         const { gigId } = this.props.match.params
+        let urlParams = new URLSearchParams(this.props.location.search);
+        this.features = urlParams.get("features")
+
+        this.fixFeatures()
         await this.loadGig(gigId)
         this.loadOwner(this.state.gig.owner._id)
+    }
+
+    fixFeatures = () => {
+        this.features = this.features.split(',')
     }
 
     loadGig = async (gigId) => {
@@ -39,26 +48,80 @@ export class Checkout extends React.Component {
         return (acc / owner.reviews.length).toFixed(1)
     }
 
+    trimIWill = () => {
+        let { gig: { title } } = this.state
+        title = title.trim();
+        var titleToEdit = title.toLowerCase();
+        if (titleToEdit.startsWith('i will')) {
+            title = title.slice(7);
+            title = title.charAt(0).toUpperCase() + title.slice(1);
+        }
+        return title;
+    }
+
+    onSetOrder = () => {
+
+    }
+
     render() {
         const { gig, owner } = this.state
-        console.log('order', owner);
         if (!gig || !owner) return <React.Fragment></React.Fragment>
         return (
-            <section className='checkout' >
+            <section className='checkout max-width-container equal-padding' >
                 <div style={{ height: '80px' }}></div>
-                <div className="main-content-container">
-                    <div className="img-container">
-                        <img src={gig.imgUrls[0]} alt="first image"></img>
+                <div className="left-side-container">
+                    <div className="main-content-container">
+                        <div className="img-container">
+                            <img src={gig.imgUrls[0]} alt="first image"></img>
+                        </div>
+                        <div className="main">
+                            <h3>{gig.title}</h3>
+                            <div className="star-rate-container">
+                                <UserStarRate owner={owner.reviews} gig={gig} isReviews={true} />
+                                <span className='num-of-rating'>{this.getAvgRate()}</span>
+                                <span className='review-length'>({owner.reviews.length})</span>
+                            </div>
+                        </div>
+                        <div className="price">{gig.price.toLocaleString("USA", { style: "currency", currency: "USD" })}</div>
                     </div>
-                    <div className="main">
-                        <h3>{gig.title}</h3>
-                        <div className="star-rate-container">
-                            <UserStarRate owner={owner.reviews} gig={gig} isReviews={true} />
-                            <span className='num-of-rating'>{this.getAvgRate()}</span>
-                            <span className='review-length'>({owner.reviews.length})</span>
+                    <div className="details">
+                        <h2>Order Details</h2>
+                        <p>{this.trimIWill()}</p>
+                        <div className='order-features'>
+                            <ul className='clean-list'>
+                                {this.features.map((feature, idx) => {
+                                    return (<li key={idx}>
+                                        <CheckIcon className="check-icon" />
+                                        {feature}
+                                    </li>)
+                                })}
+                            </ul>
                         </div>
                     </div>
-                    <div className="price">{gig.price.toLocaleString("USA", { style: "currency", currency: "USD" })}</div>
+                </div >
+                <div className="right-side-container">
+                    <div className="order-modal sticky">
+                        <h3>Price summary</h3>
+                        <ul className='clean-list'>
+                            <li>Subtotal
+                                <span> {gig.price.toLocaleString("USA", { style: "currency", currency: "USD" })}</span>
+                            </li>
+                            <li>Service Fee
+                                <span> {(gig.price / 3).toLocaleString("USA", { style: "currency", currency: "USD" })}</span>
+                            </li>
+                            <li className='bold'>
+                                Total
+                                <span> {(gig.price + (gig.price / 3)).toLocaleString("USA", { style: "currency", currency: "USD" })}</span>
+                            </li>
+                            <li> Delivery Time
+                                <div className="li-inner-container">
+                                    <span>{gig.daysToMake}</span>
+                                    <span> {gig.daysToMake === 1 ? 'day' : 'days'}</span>
+                                </div>
+                            </li>
+                        </ul>
+                        <button className='btn' onClick={this.onSetOrder}>Purchase</button>
+                    </div>
                 </div>
             </section>
         )
--- front-end/src/services/gig.service.js
+++ front-end/src/services/gig.service.js
@@ -2,82 +2,10 @@ import { storageService } from "./async-storage.service.js";
 import { utilService } from "./util.service.js";
 import { httpService } from "./http.service.js";
 import { userService } from "./user.service.js";
-// import { utilService } from './util.service.js'
-// import { userService } from './user.service.js'
+
 
 const STORAGE_KEY = "gig_db";
-// const categories = [
-//   {
-//     category: "logo design",
-//     features: [
-//       "1 Concept Included",
-//       "Logo Transparency",
-//       "Vector File",
-//       "Printable File",
-//       "3D Mockup",
-//       "Source File",
-//     ],
-//   },
-//   {
-//     category: "wordpress",
-//     features: [
-//       "1 Page",
-//       "Design Customization",
-//       "Content Upload",
-//       "Responsive Design",
-//     ],
-//   },
-//   {
-//     category: "voice over",
-//     features: ["HQ Audio File (WAV format)", "Number Of Words: 150"],
-//   },
-//   {
-//     category: "video explainer",
-//     features: ["Background Music", "Add Logo", "60 Seconds Running Time"],
-//   },
-//   {
-//     category: "social media",
-//     features: [
-//       "1 Platform",
-//       "Page Setup",
-//       "Profile Image & Cover",
-//       "Website Integration",
-//     ],
-//   },
-//   {
-//     category: "programming",
-//     features: [
-//       "Include Source Code",
-//       "Database Integration",
-//       "Setup File",
-//       "Detailed Code Comments",
-//       "3D Mockup",
-//       "Source File",
-//     ],
-//   },
-//   {
-//     category: "translation",
-//     features: ["Proofreading", "Document Formatting", "Number od words: 300"],
-//   },
-//   {
-//     category: "illustration",
-//     features: [
-//       "Source File",
-//       "High Resolution",
-//       "Background/Scene",
-//       "Color",
-//       "Full Body",
-//       "Commercial Use",
-//       "1 Figure",
-//     ],
-//   },
-// ];
-const descriptions = [
-  "Internet is taking over our lives, companies need a website design to promote their business online, this helps them reach out more customers, people tend to think that all the websites get the same results but in fact, Google doesn't like prebuilt websites.I started alone in 2016 and currently I'm leading a team of 5 WordPress developers to work together on current and future projects here on Dimerr.We are specialist in WordPress and best coding practices and we focus on the quality of our work.",
-  "I am a WordPress Pro who has worked on various Woocommerce Stores. My Experiences Include: Complete Woocommerce Configuration & Setup. Woocommerce Social Logins. Minor & Common Bug Fixes.Woocommerce Plugin Customization & Custom Development. Page Design For Cart, Checkout, & Shop Pages. Creating Product Grids & Attractive Widgets. Setup WPML with Woocommerce. Products and Theme Based Sliders using Slider Revolution. Theme Customization according to your requirements. and much more!  Basic Benefits Of My Service I PROMISE to deliver within 24 hours.Being a business owner, I understand The Importance of your business.By keeping that in mind, I make sure I value your Time & Cash, as much as I can.If I take an Order, It's my responsibility to provide a Quick & Quality Service.I've done hours of research on WC Tasks & I'm an Expert at what I do. Discussing the issue with me in Inbox & getting a Custom Offer is much better & appreciated than making an Order directly.",
-  "I will be your Professional Social Media Manager!  Nowadays, social media plays an important role in business growth. But do we use them properly? SMM is the most important service for each company worldwide. I provide natural and organic Management for your Online Business Accounts. I strongly believe in what I can offer and I strongly believe in what I can make you reach.",
-  "I'm your voice for your project right here on Dimerr.com. I am a Professional voice narrator with 3 Years of Experience. I'll narrate an upload, your voiceover project, whatever it might be. I do audio books, children's books, YouTube videos, narrations whiteboards, sales presentations, you name it.  Do you need a HIGHLY EXPERIENCED, PROFESSIONAL NARRATOR for your audiobook?  I will professionally narrate your audiobook to industry standards. I'll do it quickly professionally and make sure it's of the highest quality. Hey, it's your project. It deserves to be done. Right. So go ahead and check out my gigs, check out my samples. And if you have any questions, go ahead and send me a message and I'll make sure to respond back with a custom offer. Thanks so much. And I look forward to working with you.",
-];
+
 
 export const gigService = {
   query,
