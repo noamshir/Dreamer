91d33d6ffac0ac38d09e8b4d86a4eaf6bcede5ad
Noam Shir <noamshirm@gmail.com>
2022-01-25 22:20:23 +0200

    noam final tuesday

--- front-end/src/cmp/ReviewItem.jsx
+++ front-end/src/cmp/ReviewItem.jsx
@@ -1,12 +1,15 @@
+import photo from "../svg/photo3.jpg";
 export const ReviewItem = ({ review, width, imgUrl }) => {
     return (
         <div className="review-item" style={{ width: width }}>
-            <img src={imgUrl} alt="" />
+            <img src={photo} alt="" />
             <div className="review-content">
                 <div className="wrap-text">
-                <h5>{review.writers}</h5>
-                <h4>{review.company}</h4>
-                <p>{review.txt}</p>
+                    <div className="writer-info">
+                        <h5>{review.writers}</h5>
+                        <h4>{review.company}</h4>
+                    </div>
+                    <p>{review.txt}</p>
                 </div>
             </div>
         </div>
