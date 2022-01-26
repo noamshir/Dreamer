7b89dcd4416c315d1373a4fd42e091c3cf0ae6a6
Itamar Avady <itamaravady@gmail.com>
2022-01-26 10:07:57 +0200

    s

--- front-end/src/cmp/dashboard/OrdersList.jsx
+++ front-end/src/cmp/dashboard/OrdersList.jsx
@@ -1,11 +1,12 @@
 import { OrderPreview } from "./OrderPreview"
 
-export function OrdersList({ type, orders }) {
+export function OrdersList({ type, orders, user }) {
+    if (!orders.length) return <h3>No Orders yet...</h3>
     return (
         <ul className="clean-list orders-list flex column">
             {orders && orders.map((order) => {
-                return <li key={order._id} className="order-item flex">
-                    <OrderPreview order={order} type={type}/>
+                return <li key={order._id} className={type === 'seller' ? `order-item flex center` : 'order-item flex'}>
+                    <OrderPreview order={order} type={type} user={user} />
                 </li>
             })}
         </ul>
